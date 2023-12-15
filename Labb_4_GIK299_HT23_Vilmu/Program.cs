fb856849a6d50639b7f307e0e8bedceb9e2f69dc
Vili Mujkanovic <h23vilmu@du.se>
2023-12-15 16:15:56 +0100

    Updated version with menu.

--- Labb_4_GIK299_HT23_Vilmu/Program.cs
+++ Labb_4_GIK299_HT23_Vilmu/Program.cs
@@ -1,12 +1,91 @@
 ﻿
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+
 namespace Labb_4_GIK299_HT23_Vilmu
 {
-    internal class Program
+    public class Program
+
     {
         static void Main(string[] args)
+
         {
-            //Här hårdkodar jag data och kopplat in struct och enumen från de andra klasserna.
-            string FirstName = "Pablo";
+            //Här skapar jag en meny som har en whileloop med bool (true).
+            int menuPick = 0;
+            string menuSelect;
+            while (true)
+            {
+                Console.WriteLine(" [1] Add person." +
+                    "\n [2] Show persons in database." +
+                    "\n [3] Exit program.");
+
+                menuSelect = (Console.ReadLine());
+
+                //Här kommer en try-catch sats som ska felhantera programmet. Exempelvis ska man ej kunna skriva bokstäver när man ska välja alternativ.
+                try
+
+                {
+                    Console.WriteLine("please pick a alternative");
+
+                    if (!int.TryParse(menuSelect, out menuPick))
+                    {
+                        throw new FormatException("Press number 1 to 3 to advance in the menu.");
+                    }
+
+                    if (menuPick is < 1 or > 3)
+
+                    {
+                        throw new Exception("Alternative is number 1 to 3");
+                    }
+
+                    Console.WriteLine($"You pressed: {menuPick} ");
+                }
+
+
+
+                catch (ArgumentOutOfRangeException ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An unexpected error occurred: {ex.Message}");
+                }
+
+
+                switch (menuSelect)
+                {
+                    case "1":
+
+                        Console.Clear();
+                        addPerson();
+                        break;
+
+                }
+                void addPerson()
+                {
+                    int [] extraPerson;
+                  
+
+                }
+            }
+
+
+
+
+            /*
+
+
+        //Här hårdkodar jag data och kopplat in struct och enumen från de andra klasserna.
+        string FirstName = "Pablo";
             string LastName = "Escobar";
             int Age = 44;
             string Eyecolor = "Brown";
@@ -19,6 +98,8 @@ namespace Labb_4_GIK299_HT23_Vilmu
             Person person = new Person(FirstName, LastName, Age, gender, hair, birthDay, Eyecolor);
 
             Console.WriteLine(person.ToString());
+
+             */
         }
     }
 }
