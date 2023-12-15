7aa468b0b8707719deb9e79fd597045a7309ac72
Vili Mujkanovic <h23vilmu@du.se>
2023-12-15 13:05:03 +0100

    Add project files.

---
+++ Labb_4_GIK299_HT23_Vilmu/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Labb_4_GIK299_HT23_Vilmu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
